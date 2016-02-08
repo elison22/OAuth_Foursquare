8c2d90746ddfba842bcb4336c09a1f6596dd8264
Brandt Elison <behatdude@gmail.com>
2016-02-08 13:31:58 -0700

    Initial Commit

---
+++ OAuth_Foursquare/HomeModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OAuth_Foursquare
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello Nancy from Pluralsight";
+            Get["/home"] = _ => View["home"];
+        }
+    }
+}
