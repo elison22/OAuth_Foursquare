cd846f5ea43acdaa9fc7e776546130266c28b06f
Brandt Elison <behatdude@gmail.com>
2016-02-09 11:32:12 -0700

    Revert "preparing to revert"
    This reverts commit e15aba4961189fc830eace709372992d9fe84406.
    
    Part1 of removing the NancyUserManager files

---
+++ OAuth_Foursquare/HomeModule.cs
@@ -0,0 +1,47 @@
+﻿using Nancy;
+using OAuth_Foursquare.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OAuth_Foursquare
+{
+    public class HomeModule : NancyModule
+    {
+
+        List<User> tempUsers = new List<User>
+        {
+            new User
+            {
+                FirstName = "Brandt",
+                LastName = "Elison",
+                Username = "elison22"
+            },
+            new User
+            {
+                FirstName = "Katrina",
+                LastName = "Elison",
+                Username = "kdog0128"
+            },
+            new User
+            {
+                FirstName = "Carl",
+                LastName = "Walsh",
+                Username = "darthwalsh"
+            }
+        };
+
+        public HomeModule() 
+        {
+            Get["/"] = _ => "Hello Nancy from Pluralsight";
+            Get["/user"] = _ => View["users", tempUsers];
+            Get["/user/{name}"] = p => View["user", tempUsers.FirstOrDefault(x => MatchingUser(x, p.name))];
+        }
+
+        Func<User, string, bool> MatchingUser = (user, name) =>
+              user.FirstName == name ||
+              user.LastName == name ||
+              user.Username == name;
+    }
+}
